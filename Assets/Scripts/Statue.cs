6d42496a3c1045c551506fdcd0017c84279f5d4a
AntiProgresant <alexanderlane614@gmail.com>
2021-04-26 17:38:00 -0400

    Final

--- Assets/Scripts/Statue.cs
+++ Assets/Scripts/Statue.cs
@@ -6,33 +6,18 @@ using UnityEngine;
 public class Statue : MonoBehaviour
 {
     public GameObject player;
-    public AudioClip[] stoneSounds;
 
-    private NavMeshAgent nav;
-    private AudioSource sound;
-    public Animator anim;
+    private NavMeshAgent Nav;
 
     // Start is called before the first frame update
     void Start()
     {
-        nav = GetComponent<NavMeshAgent>();
-        sound = GetComponent<AudioSource>();
-        anim.GetComponent<Animator>();
-    }
-
-    public void footstep(int _num)
-    {
-        sound.clip = stoneSounds[_num];
-        sound.Play();
+        Nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        anim.SetFloat("velocity", nav.velocity.magnitude);
-        // anim.SetBool("isWalking", true);
-        
-        nav.SetDestination(player.transform.position);
-        
+        Nav.SetDestination(player.transform.position);
     }
 }
