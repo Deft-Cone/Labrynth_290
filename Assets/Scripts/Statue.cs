f44f30f3b969bda9bb3398975b8a1112b4802bb0
AntiProgresant <alexanderlane614@gmail.com>
2021-04-26 03:28:23 -0400

    Dude chases you

---
+++ Assets/Scripts/Statue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Statue : MonoBehaviour
+{
+    public GameObject player;
+
+    private NavMeshAgent Nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Nav.SetDestination(player.transform.position);
+    }
+}
