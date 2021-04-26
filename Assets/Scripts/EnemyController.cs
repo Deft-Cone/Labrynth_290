c2c21105f737be5fbf7bc721adaefd0415e43928
AntiProgresant <alexanderlane614@gmail.com>
2021-04-26 15:16:30 -0400

    What

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,36 +4,15 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public GameObject Enemy;
-    public GameObject Player;
-    public float Speed;
-    public bool isLooking;
-
-
+    // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        RaycastHit Hit;
-        Ray playerGaze = new Ray(Player.transform.position, Player.transform.forward);
-
-        if(Physics.Raycast(playerGaze, out Hit))
-        {
-            if(Hit.collider.tag.Equals("Enemy"))
-            {
-                Speed = 0f;
-            }
-            else
-            {
-                Speed = 5f;
-            }
-        }
-
-        transform.position = Vector3.MoveTowards (transform.position, Player.transform.position, Speed * Time.deltaTime);
+        
     }
-
 }
